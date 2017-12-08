38bdcd8217df39ff9a077743f5d28ed427aa0c86
Yuki Matsuyama <matsumatsu20@gmail.com>
2017-12-09 06:30:13 +0900

    hoge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -106,81 +106,75 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     this.isBrowser = isBrowser;
 
-    let source = this.route.params
-      .switchMap((params: Params) => {
-        let id = typeof params['id'] === 'undefined' ? 1 : +params['id'];
-        return Observable.of(id);
+    let id = this.session.get('team_id');
+
+    this.model.get(`/api/teams/${id}`)
+      .subscribe((teamData: { id: number; name: string; members: string[] }) => {
+        let ms = teamData.members.map((member) => {
+          return { name: member };
+        });
+        this.team = new Team(teamData.id, teamData.name, ms);
       });
 
-    source.subscribe((id) => {
-      this.model.get(`/api/teams/${id}`)
-        .subscribe((teamData: { id: number; name: string; members: string[] }) => {
-          let ms = teamData.members.map((member) => {
-            return { name: member };
-          });
-          this.team = new Team(teamData.id, teamData.name, ms);
-        });
-
-      this.model.get(`/api/scores/${id}`)
-        .subscribe((data: { id: number; score: number; message: string; date: string }[]) => {
-          this.graphOptions = {
-            size: { height: 400 },
-            axis: {
-              x: {
-                type: 'timeseries',
-                localtime: true,
-                tick: {
-                  format: (x: any) => {
-                    // localeが適応されないためハック
-                    let d = new Date(x);
-                    d.setTime(d.getTime() - d.getTimezoneOffset() * 60 * 1000);
-                    let day = '日月火水木金土'[d.getDay()];
-                    return `${d.getMonth() + 1}/${d.getDate()}(${day})`;
-                  },
-                  culling: { max: 6 },
-                  count: 6
-                }
-              },
-              y: {
-                min: 0,
-                padding: 0
-              }
-            },
-            tooltip: {
-              format: {
-                title: (x) => {
+    this.model.get(`/api/scores/${id}`)
+      .subscribe((data: { id: number; score: number; message: string; date: string }[]) => {
+        this.graphOptions = {
+          size: { height: 400 },
+          axis: {
+            x: {
+              type: 'timeseries',
+              localtime: true,
+              tick: {
+                format: (x: any) => {
                   // localeが適応されないためハック
                   let d = new Date(x);
                   d.setTime(d.getTime() - d.getTimezoneOffset() * 60 * 1000);
                   let day = '日月火水木金土'[d.getDay()];
-                  return `${d.getMonth() + 1}/${d.getDate()}(${day}) ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-                }
+                  return `${d.getMonth() + 1}/${d.getDate()}(${day})`;
+                },
+                culling: { max: 6 },
+                count: 6
               }
             },
-            subchart: {
-              show: true
-            },
-            zoom: {
-              enabled: true,
-              rescale: true,
-              extent: [1, 10]
-            },
-            data: {
-              type: 'line',
-              x: 'x',
-              xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-              columns: [
-                [`x`, ...data.map((s) => s.date).reverse()],
-                [`${this.team.name}`, ...data.map((s) => s.score).reverse()]
-              ]
+            y: {
+              min: 0,
+              padding: 0
             }
-          };
-
-          if (this.isOwnTeam()) {
-            this.scores = data.map((s) => new Score(s.score, s.message, s.date));
+          },
+          tooltip: {
+            format: {
+              title: (x) => {
+                // localeが適応されないためハック
+                let d = new Date(x);
+                d.setTime(d.getTime() - d.getTimezoneOffset() * 60 * 1000);
+                let day = '日月火水木金土'[d.getDay()];
+                return `${d.getMonth() + 1}/${d.getDate()}(${day}) ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+              }
+            }
+          },
+          subchart: {
+            show: true
+          },
+          zoom: {
+            enabled: true,
+            rescale: true,
+            extent: [1, 10]
+          },
+          data: {
+            type: 'line',
+            x: 'x',
+            xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+            columns: [
+              [`x`, ...data.map((s) => s.date).reverse()],
+              [`${this.team.name}`, ...data.map((s) => s.score).reverse()]
+            ]
           }
-        });
-    });
+        };
+
+        if (this.isOwnTeam()) {
+          this.scores = data.map((s) => new Score(s.score, s.message, s.date));
+        }
+      });
 
     this.model.get('/api/queues').subscribe(
       (data) => {
